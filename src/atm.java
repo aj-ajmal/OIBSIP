a85e03c1c06a7ba8c736c1f5cabeaf7c57ca6213
Ajmal <syedajmal272@gmail.com>
2024-11-10 23:53:10 +0530

    test

--- src/atm.java
+++ src/atm.java
@@ -177,7 +177,7 @@ class ATMSystem {
 
 public class atm {
     public static void main(String[] args) {
-        ATMSystem atmSystem =    new ATMSystem();
+        ATMSystem atmSystem =     new ATMSystem();
         atmSystem.start(); //completed
 
     }
